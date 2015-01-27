3e2ecef060881c9a1a388bea18cce9083b1bbd96
Jmcchap <Jmcchap@gmail.com>
2015-01-27 00:20:19 -0600

    Final Pin Updates (II)

--- Lab2Jessie.c
+++ Lab2Jessie.c
@@ -1,71 +1,81 @@
-/*====================================================*/
-/* Jessie Chapman */
-/* ELEC 3040 - Lab 2, Program 1 */
-/* Decimal counter controled by two switches */
-/*====================================================*/
-#include "STM32L1xx.h" /* Microcontroller information */
-/* Define global variables */
-int toggles; /* number of times LED state toggled */
-int count; /*the current number displayed*/
+#include "STM32L1xx.h"
 
-/*---------------------------------------------------*/
-/* Initialize GPIO pins used in the program */
-/* PA0 = push button */
-/* PC8 = blue LED, PC9 = green LED */
-/*---------------------------------------------------*/
-
-void PinSetup () {
- /* Configure PA0 as input pin to read push button */
- RCC->AHBENR |= 0x01; /* Enable GPIOA clock (bit 0) */
- GPIOA->MODER &= ~(0x00000003); /* General purpose input mode */
- GPIOA->OSPEEDR &= ~(0x00000003); /* Clear PA0 speed bits */
- GPIOA->OSPEEDR |= (0x00000002); /* 40 MHz Medium speed */
- GPIOA->PUPDR &= ~(0x00000003); /* No pull-up, pull-down */
- /* Configure PC8,PC9 as output pins to drive LEDs */
- RCC->AHBENR |= 0x04; /* Enable GPIOC clock (bit 2) */
- GPIOC->MODER &= ~(0x000F0000); /* Clear PC9-PC8 mode bits */
- GPIOC->MODER |= (0x00050000); /* General purpose output mode*/
- GPIOC->OTYPER &= ~(0x00000300); /* Output push-pull */
- GPIOC->OSPEEDR &= ~(0x000F0000); /* Clear PC9-PC8 speed bits */
- GPIOC->OSPEEDR |= (0x00050000); /* 2 MHz Low speed */
- GPIOC->PUPDR &= ~(0x000F0000); /* No pull-up, pull-down */
-}
+int count = 0;
 
 /*----------------------------------------------------------*/
-/* Delay function - do nothing for about 1 second */
+/* Pin setup - initialize GPIO pins used in program */
+/* PA1 - Switch 1 connected to DIO0 on the EEBoard */
+/* PA2 - Switch 2 connected to DIO1 on the EEBoard */
 /*----------------------------------------------------------*/
-void delay () {
- int i,j;
- for (i=0; i<100; i++) { //outer loop
- for (j=0; j<20000; j++) { //inner loop
- } //do nothing
+ void PinSetup(){
+ /*Configure PC() as output pins for LEDS*/
+ RCC->AHBENR |= 0x04;             /*Enable GPIOC clock*/
+ GPIOC->MODER &= (0xFFFFFF00);    /*Clear Pins 0-3 to be set as output*/
+ GPIOC->MODER |= (0x00000055);    /*Sets Pins 0-3 in output mode*/
+
+ /*Configure PA() as input pins for S1 and S2*/
+ RCC->AHBENR |= 0x01;             /*Enable GPIOA clock*/
+ GPIOA->MODER &= (0xFFFFFFC3);    /*Clears Pins 1 and 2 as inputs*/
+
+ }
+
+/*----------------------------------------------------------*/
+/* Delay function - 0.5 second delay??? */
+/*----------------------------------------------------------*/
+void delay ()
+{
+ int a,b;
+ for (a=0; a<100; a++) { //outer loop
+ for (b=0; b<10000; b++) { //inner loop
+ }
  }
 }
 
-/*------------------------------------------------*/
+/*----------------------------------------------------------*/
+/* Counting function - reversible decade counter with LED display */
+/*----------------------------------------------------------*/
+void counting (unsigned char sw2)
+{
+  if (sw2 = 0) //count up
+  {
+    if (count>=9)
+      count = 0; //if count=9 roll over to 0
+      else
+      count++; //increment counter by one
+  }
+    else //count down
+  {
+    if (count <= 0)
+      count = 9; //if count=0 roll over to 9
+      else
+      count--; //decrement counter by one
+  }
+  //insert smart stuff about binary and LEDs here
+}
+
+/*----------------------------------------------------------*/
 /* Main program */
-/*------------------------------------------------*/
-int main(void) {
- unsigned char sw1; //state of SW1
- unsigned char led1; //state of LED1
- PinSetup(); //Configure GPIO pins
- led1 = 0; //Initial LED state
- toggles = 0; //#times LED state changed
+/*----------------------------------------------------------*/
+int main(void)
+{
+  unsigned char sw1 = 1; //state of SW1
+  unsigned char sw2 = 0; //state of SW2
+  unsigned char led1 = 0; //state of LED1
+  unsigned char led2 = 0; //state of LED2
+  unsigned char led3 = 0; //state of LED3
+  unsigned char led4 = 0; //state of LED4
+  PinSetup(); //Configure GPIO pins
 
- /* Endless loop */
- while (1) { //Can also use: for(;;) {
-if (led1 == 0) //LED off?
-GPIOC->BSRRH = 0x0100; //Reset PC8=0 to turn OFF blue LED
-else //LED on
-GPIOC->BSRRL = 0x0100; //Set PC8=1 to turn ON blue LED
- sw1 = GPIOA->IDR & 0x01; //Read GPIOA and mask all but bit 0
-
- /* Wait in loop until SW1 pressed */
- while (sw1 == 0) { //Wait for SW1 = 1 on PE0
-sw1 = GPIOA->IDR & 0x01; //Read GPIOA and mask all but bit 0
- }
- delay(); //Time delay for button release
- led1 = ~led1; //Complement LED1 state
-toggles++; //Increment #times LED toggled
- } /* repeat forever */
-} 
+  while(1) //Endless loop
+  {
+    //insert smart stuff about determining the values of sw1 and sw2 here
+    if (sw1 = 1) //START - increment counter and delay 0.5 seconds
+    {
+      counting(sw2); //increment counter and display values with LED
+      delay(); //0.5 second delay
+    }
+    else //STOP - do nothing
+    {
+    }
+  }
+}
